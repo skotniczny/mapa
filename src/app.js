d96efe8cf9720ee4d0a75ff81d9b7e2143263050
Łukasz <skotniczny@outlook.com>
2024-05-15 22:02:14 +0200

    Dodanie sterowania mapą za pomocą klawiatury

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
-const el = document.createElement('div')
-el.textContent = 'Mapa Świata'
-document.body.appendChild(el)
+import { svgPositionGet, svgPositionSet } from './js/svg-utils.js'
+
+const map = document.querySelector('svg')
+const canvas = map.querySelector('#canvas')
+
+const handleKeyboard = event => {
+  if (event.target.tagName.toLowerCase() !== 'input') {
+    const position = svgPositionGet(canvas)
+    const key = event.key.toLowerCase()
+    if (key === 'arrowright' || key === 'd') { svgPositionSet(canvas, { x: position.x - 50, y: position.y }) }
+    if (key === 'arrowleft' || key === 'a') { svgPositionSet(canvas, { x: position.x + 50, y: position.y }) }
+    if (key === 'arrowup' || key === 'w') { svgPositionSet(canvas, { x: position.x, y: position.y + 50 }) }
+    if (key === 'arrowdown' || key === 's') { svgPositionSet(canvas, { x: position.x, y: position.y - 50 }) }
+  }
+}
+
+document.addEventListener('keydown', handleKeyboard)
---
+++ src/js/svg-utils.js
@@ -0,0 +1,52 @@
+/* global SVGTransform */
+
+function first (iterable, filter) {
+  for (const el of iterable) {
+    if (!filter || filter(el)) { return el }
+  }
+  return null
+}
+
+function svgPositionSet (svgEl, position, svg) {
+  ensureTransform(svgEl, SVGTransform.SVG_TRANSFORM_TRANSLATE, svg).setTranslate(position.x, position.y)
+}
+
+/**
+  * @param {SVGGraphicsElement} svgEl
+  * @returns { {x: number, y: number} }
+  */
+function svgPositionGet (svgEl) {
+  const tr = first(svgEl.transform.baseVal, tt => tt.type === SVGTransform.SVG_TRANSFORM_TRANSLATE)
+  return tr ? { x: tr.matrix.e, y: tr.matrix.f } : { x: 0, y: 0 }
+}
+
+function ensureTransform (svgEl, transform, svg) {
+  let tr = first(svgEl.transform.baseVal, tt => tt.type === transform)
+  if (!tr) {
+    tr = (svgEl.ownerSVGElement || svg).createSVGTransform()
+    svgEl.transform.baseVal.appendItem(tr)
+  }
+  return tr
+}
+
+/**
+  * @param {SVGGraphicsElement} svgEl
+  * @param {Point} fixedPoint
+  *                this point will not change position while scale
+  * @param {number} scale
+  * @param {number} nextScale
+  */
+
+function svgScale (svgEl, fixedPoint, scale, nextScale) {
+  const position = svgPositionGet(svgEl)
+
+  svgPositionSet(svgEl, {
+    x: nextScale / scale * (position.x - fixedPoint.x) + fixedPoint.x,
+    y: nextScale / scale * (position.y - fixedPoint.y) + fixedPoint.y
+  })
+
+  ensureTransform(svgEl, SVGTransform.SVG_TRANSFORM_SCALE)
+    .setScale(nextScale, nextScale)
+}
+
+export { svgPositionSet, svgPositionGet, svgScale }
